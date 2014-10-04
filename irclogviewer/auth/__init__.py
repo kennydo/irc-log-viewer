448fef13745d42c5536b7ef47ef15bb4378e1ad4
Kenny Do <kedo@ocf.berkeley.edu>
2014-10-04 01:56:51 -0700

    after user logs in, redirect them to the original page

--- irclogviewer/auth/__init__.py
+++ irclogviewer/auth/__init__.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, redirect, request, session, url_for
+from urllib.parse import urlparse
+from flask import Blueprint, current_app, redirect, request, session, url_for
 from flask_oauthlib.client import OAuth
 
 
@@ -18,6 +19,20 @@ google = oauth.remote_app(
 )
 
 
+def is_safe_next_url(next_url):
+    """Returns True if the given ``next_url`` is safe to redirect to.
+    We consider a URL to be safe to redirect to if and only if
+    the scheme (http/s) and the netloc (domain.name.com:port) match.
+    """
+    parsed_next = urlparse(next_url)
+    parsed_host = urlparse(request.host_url)
+    if parsed_host.scheme != parsed_next.scheme:
+        return False
+    if parsed_host.netloc != parsed_next.netloc:
+        return False
+    return True
+
+
 @auth.record_once
 def init_google(state):
     oauth.init_app(state.app)
@@ -31,7 +46,18 @@ def get_google_oauth_token():
 @auth.route('/')
 @auth.route('/login')
 def login():
-    return google.authorize(callback=url_for('.authorized', _external=True))
+    next_url = request.args.get('next')
+    if next_url:
+        if is_safe_next_url(next_url):
+            # Since passing along the next_url as a GET param requires
+            # a different callback for each page, and Google requires us to
+            # whitelist each allowed callback page, we can't pass it as a GET
+            # param. Instead, we sanitize and put into the session.
+            session['next_url'] = next_url
+        else:
+            current_app.logger.warning("Received unsafe next url: " + next_url)
+    return google.authorize(
+        callback=url_for('.authorized', _external=True))
 
 
 @auth.route('/logout')
@@ -51,4 +77,5 @@ def authorized(resp):
         )
     session['google_token'] = (resp['access_token'], '')
     session['user'] = google.get('userinfo').data
-    return redirect(url_for('logs.index'))
+    next_url = session.pop('next_url', url_for('logs.index'))
+    return redirect(next_url)
